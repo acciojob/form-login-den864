0caebb8e4a4a88d3f9ba2f317495aca23f1ce22b
Dhaval Birade <69907022+den864@users.noreply.github.com>
2025-09-09 08:34:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+    event.preventDefault();
 
+    const form=document.getElementById("form1");
+    const data=new FormData(form);
+
+    const fname=data.get("fname").trim();
+    const lname=data.get("lname").trim();
+
+    if(!fname || !lname) return;
+
+    alert(`${fname} ${lname}`);
 }
+
